7d7c7bb004b4fab638b73c6bb874e1ee5f167dfc
Your Name <realfasih@gmail.com>
2022-02-13 12:13:07 +0500

    completed

---
+++ img/Paid membership Blog - REAL WordPress Theme Preview - ThemeForest.html YZA_files/theme-style.php
@@ -0,0 +1,261 @@
+	.blog-unit-tit a:hover,.count-box,.social-like .wpulike .counter a.image:before,.post-meta-social .count,.entry .pmpro_box p a:hover, .list-author-unit .socialmeida-a:hover,.height-light-ux,
+	a:hover,.entry p a,.sidebar_widget a:hover,#footer a:hover,.archive-tit a:hover,.text_block a,.post_meta > li a:hover, #sidebar a:hover, #comments .comment-author a:hover,#comments .reply a:hover,.fourofour-wrap a,.archive-meta-unit a:hover,.post-meta-unit a:hover,#back-top:hover,.heighlight,.archive-meta-item a,.author-name,.archive-unit-h2 a:hover,
+	.carousel-wrap a:hover,.blog-item-main h2 a:hover,.related-post-wrap h3:hover a,.ux-grid-tit-a:hover,.iconbox-a .iconbox-h3:hover,.iconbox-a:hover,.iocnbox:hover .icon_wrap i.fa,.blog-masony-item .item-link:hover:before,.clients_wrap .carousel-btn .carousel-btn-a:hover:before,
+	.blog_meta a:hover,.breadcrumbs a:hover,.link-wrap a:hover,.archive-wrap h3 a:hover,.more-link:hover,.post-color-default,.latest-posts-tags a:hover,.pagenums .current,.page-numbers.current,.fullwidth-text-white .fullwrap-with-tab-nav-a:hover,.fullwrap-with-tab-nav-a:hover,.fullwrap-with-tab-nav-a.full-nav-actived,.fullwidth-text-white .fullwrap-with-tab-nav-a.full-nav-actived
+	{ 
+		color:#DFBD83; 
+	}
+	.post-meta-social-li .share:hover,#footer .socialmeida-a:hover:before,
+	.tw-style-a:hover,button:hover, input[type="submit"]:hover,.pmpro_btn:hover,#content_wrap .pmpro_content_message a:hover,.archive-list .pmpro_content_message a:hover,.member-tip,
+	.tagcloud a:hover,.related-post-wrap h3:before,#back-top:hover .back-top-icon:before,#back-top:hover .back-top-icon:after, #back-top:hover:before, #back-top:hover:after,.single-image-mask,
+	input.idi_send:hover,.ux-hover-icon-wrap,.iconbox-content-hide .icon_text,.process-bar,.nav-tabs > li > a:hover,.portfolio-caroufredsel-hover
+	{ 
+		background-color:#DFBD83;
+	}
+
+	
+	textarea, select, input[type="text"],input[type="password"],input[type="email"],input[type="url"],.post-meta-social-li .share,.post_social:before, .post_social:after,.break-line,.tagcloud a,
+	.slider-panel,#main_title_wrap,.nav-tabs > li,.promote-wrap,.process-bar-wrap,.post_meta,.pagenumber a,.countdown_section,.interlock-item,.standard-blog-link-wrap,.blog-item.quote,.portfolio-standatd-tit,.quote-wrap,.entry pre,.text_block pre,
+	.pagenumber span,.testimenials,.testimenials .arrow-bg,.accordion-heading,.testimonial-thum-bg,.single-feild,.fullwidth-text-white .iconbox-content-hide .icon_wrap
+	{ 
+		background-color: #EEEEEE; 
+	}
+	.progress_bars_with_image_content .bar .bar_noactive.grey, 
+	.standard-list-item:hover .portfolio-standatd-tit-a,.standard-list-item:hover .portfolio-standatd-tags a[rel*="tag"]
+	{
+	  color: #EEEEEE; 
+	}
+	.border-style2,.border-style3,.nav-tabs > li > a,.tab-content,.nav-tabs > .active > a, .nav-tabs > .active > a:hover, .nav-tabs > .active > a:focus,.tabs-v,.single-feild,.archive-unit,.widget_footer,
+	.list-author-unit,li.commlist-unit,textarea,select, input[type="text"], input[type="password"], input[type="datetime"], input[type="datetime-local"], input[type="date"], input[type="month"], input[type="time"], input[type="week"], input[type="number"], input[type="email"], input[type="url"], input[type="search"], input[type="tel"], input[type="color"], .uneditable-input
+	{ 
+		border-color: #EEEEEE; 
+	}
+	
+	.nav.nav-tabs, .tabs-v .nav-tabs > li:last-child.active>a,#content .rotatingtweets, #content .norotatingtweets {
+		border-bottom-color: #EEEEEE; 
+	}
+	.tab-content.tab-content-v,blockquote {
+		border-left-color: #EEEEEE; 
+	}
+	.blog-unit,.search-result-unit,
+	.tabs-v .nav-tabs > .active > a,#content .rotatingtweets, #content .norotatingtweets {
+		border-top-color: #EEEEEE; 
+	}
+	
+	
+	.logo-h1  { 
+		color:#FFFFFF 
+	}
+	#qLbar,#site-loading-mask {
+		background-color: #FFFFFF!important; 
+	}
+	
+	
+	#footer .logo-h1  { 
+		color:#E0E0E0 
+	}
+	
+
+	#navi a,
+	.subscribe-link-a,
+	.search-top-btn-class,
+	.subscribe,
+	#header-main .socialmeida-a,
+	#hidden-panel .socialmeida-a,
+	.social-header-triggle,
+	#hp-menu-wrap a,
+	#header-main .socialmeida-a:hover,
+	#hidden-panel .socialmeida-a:hover,
+	#hidden-panel .socialmeida-a:hover { 
+		color: #EBEBEB; 
+	}
+	
+	#hidden-panel .socialmeida-a:before,
+	.menu-item-has-children>a:before {
+		background-color: #EBEBEB; 
+	}
+
+
+	#navi .current-menu-item > a,
+	#navi a:hover,
+	#navi ul.sub-menu a:hover,
+	#navi>div>ul li.current-menu-parent>a,
+	#navi>div>ul>li.current-menu-ancestor>a,
+	#navi .sub-menu li.current-menu-item>a,
+	#hidden-panel #navi .current-menu-item > a,
+	#hidden-panel #navi a:hover,
+	#hidden-panel #navi>div>ul li.current-menu-parent>a,
+	#hidden-panel #navi>div>ul>li.current-menu-ancestor>a,
+	#hidden-panel #navi .sub-menu li.current-menu-item>a,
+	#hp-menu-wrap .current-menu-item > a,
+	#hp-menu-wrap a:hover,
+	#hp-menu-wrap > ul li.current-menu-parent>a,
+	#hp-menu-wrap >ul>li.current-menu-ancestor>a,
+	#hp-menu-wrap .sub-menu li.current-menu-item>a,
+	.subscribe-link-a:hover,
+	#header-main .socialmeida-a:hover,
+	.subscribe-link-a:hover,
+	.search-top-btn-class:hover
+	{ 
+		color:#DFBD83; 
+	}
+	#hidden-panel .socialmeida-a:hover:before,
+	.menu-item-has-children>a:hover:before,
+	#navi .current-menu-item.menu-item-has-children > a:before,
+	#navi>div>ul li.current-menu-parent.menu-item-has-children>a:before,
+	#navi>div>ul>li.current-menu-ancestor.menu-item-has-children>a:before,
+	#navi .sub-menu li.current-menu-item.menu-item-has-children>a:before {
+		background-color: #DFBD83;
+	}
+	
+
+
+	#navi ul.sub-menu a { color:#EBEBEB; }
+
+	
+	.blog-unit-tit a,.main-title,.site-loading-logo .logo-h1,#comments .comment-author a,h1,h2,h3,h4,h5,h6,.archive-tit a,.blog-item-main h2 a,.item-title-a,#sidebar .social_active i:hover,.countdown_amount,.ux-grid-tit-a,.filters.filters-nobg li a:hover,.filters.filters-nobg li.active a,.portfolio-standatd-tit-a,.portfolio-standatd-tags a[rel*="tag"],.archive-unit-h2 a,.archive-date,
+	.nav-tabs > .active > a, .nav-tabs > .active > a:hover, .nav-tabs > .active > a:focus,.accordion-heading .accordion-toggle,.post-navi-a,.moudle .ux-btn,
+	.jqbar.vertical span,.team-item-con-back a,.team-item-con-back i,.team-item-con-h p,.slider-panel-item h2.slider-title a,.bignumber-item.post-color-default,.blog-item .date-block,
+	input[type="text"],input[type="password"], textarea,.mfp-title,.mfp-arrow-right:before,.mfp-arrow-left:before,.clients_wrap .carousel-btn .carousel-btn-a,.gallery-info-property-item
+	{ 
+		color:#28282E; 
+	}
+	
+	.gallery-wrap-fullwidth .gallery-info-property,.accordion-heading,.title-ux.line_under_over,.gallery-info-property, .gallery-wrap-sidebar .entry, .social-share,
+	.moudle .ux-btn:hover { 
+		border-color: #28282E; 
+	}
+	
+	h1.main-title:before,.team-item-con,.ux-btn:hover,.title-ux.line_both_sides:before,.title-ux.line_both_sides:after,.audio_player_list,.galleria-info,#float-bar-triggler,.float-bar-inn,.short_line:after,
+	button, input[type="submit"],.promote-button:hover,#content_wrap .pmpro_content_message a,.archive-list .pmpro_content_message a,.accordion-style-b .accordion-heading a:before,.accordion-style-b .accordion-heading a:after,.separator_inn.bg-,.portfolio-standatd-tit:before 
+	{
+	  background-color: #28282E;
+	}
+	#qLpercentage {
+		color:#28282E!important; 
+	}
+
+	.ux-mobile #navi,.magazine-unit.magazine-bgcolor-default,.magazine-unit.magazine-bgcolor-default a,.entry .pmpro_box p a, 
+	body,a,.entry p a:hover,.text_block a:hover,#content_wrap,#comments,.blog-item-excerpt,.archive-unit-excerpt,.mfp-image-holder .mfp-close, .mfp-iframe-holder .mfp-close,.archive-meta-item a:hover,.entry code,.text_block code,
+	h3#reply-title small, #comments .nav-tabs li.active h3#reply-title .logged,#comments .nav-tabs li a:hover h3 .logged,.testimonial-thum-bg i.fa,
+	.header-info-mobile,.carousel-wrap a.disabled:hover { 
+	  color: #313139; 
+	}
+	.filters.filters-nobg li a:before,.blog-item-more-a:hover,.tw-style-a:before,.tw-style-a:after
+	{
+		background-color: #313139; 
+	}
+	.blog-item-more-a:hover {
+		border-color: #313139; 
+	}
+	
+	.post-navi-unit-a,.related-posts-date,.list-author-unit .socialmeida-a,.mfp-counter,
+	.post_meta>li,.post_meta>li a,.post-meta, .post-meta a,.archive-meta-unit,.archive-meta-unit a,.latest-posts-tags a,.latest-posts-date,#comments .comment-meta .comment-reply-link,#comments .comment-meta .date,
+	#mobile-header-meta p,.bbp-meta,.bbp-meta a,.bbp-author-role,.bbp-pagination-count,span.bbp-author-ip,.bbp-forum-content,.infrographic-subtit,.blog_meta,.blog_meta a,.more-link,.blog-item-excerpt .wp-caption-text,
+	textarea,input[type="text"],input[type="password"],input[type="datetime"],input[type="datetime-local"],input[type="date"],input[type="month"],input[type="time"],input[type="week"],input[type="number"],input[type="email"],input[type="url"],input[type="search"],input[type="tel"],input[type="color"],.uneditable-input
+	{ 
+	  color:#999999; 
+	}
+	.comment-author:after {
+		background-color: #999999; 
+	}
+	
+
+    ::selection { background: #DFBD83; }
+	::-moz-selection { background: #DFBD83; }
+	::-webkit-selection { background: #DFBD83; }
+
+	
+	#header-inn-main,.page-loading,#search-overlay,
+	body,#wrap-outer,#top-wrap,#main,.separator h4,.flex-direction-nav a[class*="flex-"],.carousel-control,
+	.nav-tabs > .active > a, .nav-tabs > .active > a:hover, .nav-tabs > .active > a:focus,.tab-content,.filters.filter-floating li a:before,
+	.mfp-bg,.mfp-figure:after,.mfp-figure 
+	{ 
+	  background-color: #FFFFFF;
+	}
+	.testimenials span.arrow,.nav-tabs > .active > a, .nav-tabs > .active > a:hover, .nav-tabs > .active > a:focus { 
+		border-bottom-color: #FFFFFF; 
+	}
+	.tabs-v .nav-tabs > .active > a
+	{ 
+	  border-right-color: #FFFFFF; 
+	}
+	.post-meta-social-li .share:hover,.tw-style-a,.tw-style-a:hover,.quote-wrap,.gallery-wrap-slider .flex-direction-nav a[class*="flex-"],
+	.carousel-control,.moudle .ux-btn:hover,button, input[type="submit"],#content_wrap .pmpro_content_message a,.archive-list .pmpro_content_message a, .audio_player_list {
+	  color: #FFFFFF; 
+	}
+	
+	
+    #top-hot > .hot-img-wrap { background-color: #313139; }
+    #top-hot.cover-masker:before { background-color: #28282E; }
+	
+	.sidebar_widget h3.widget-title,
+	.sidebar_widget h3.widget-title a { 
+	  color: #28282E;
+	}
+	
+	
+	.sidebar_widget,
+	.sidebar_widget a { 
+	  color: #999999; 
+	}
+
+	#hidden-panel .sidebar_widget .widget-title,
+	#hidden-panel .sidebar_widget .widget-title a  { 
+	  color: #FFFFFF; 
+	}
+
+	
+	#hidden-panel .sidebar_widget,
+	#hidden-panel .sidebar_widget a { 
+	  color: #CCCCCC; 
+	}
+
+	
+	#hidden-panel { 
+	  background-color: #313139; 
+	}
+
+	
+	#footer,#footer a,
+	.copyright, .copyright a,
+	#footer .widget-title,#footer .widget-title a,
+	.subscribe-foot-tit,
+	#footer .socialmeida-a:hover { 
+	  color: #FFFFFF; 
+	}
+	#footer .socialmeida-a:before {
+		background-color: #FFFFFF; 
+	}
+	
+
+	
+	#footer { 
+	  background-color: #313139; 
+	}
+
+	h1,h2,h3,h4,h5,h6,#content_wrap .infrographic p,#content_wrap .promote-mod p { 
+		font-family: Droid Serif; 
+	}
+    h1,h2,h3,h4,h5,h6,#content_wrap .infrographic p,#content_wrap .promote-mod p { 
+    font-weight: 700; font-style: normal;}
+	 body, input, textarea, select, div.bbp-template-notice p, legend,.gallery-info-property-tit,
+	 #content_wrap #theme-my-login p,#content_wrap #theme-my-login li,#content_wrap #pmpro_pricing_fields p,#content_wrap #pmpro_account p,#content_wrap #pmpro_account li,#content_wrap .pmpro_form p,#content_wrap .pmpro_form li,
+	 .pmpro-confirmation #content_wrap .entry p,.pmpro-confirmation .entry li,.pmpro-invoice .entry li { 
+		font-family: Alike; 
+	}
+     body, input, textarea, div.bbp-template-notice p, legend,.gallery-info-property-tit,
+     #content_wrap #theme-my-login p,#content_wrap #theme-my-login li,#content_wrap #pmpro_pricing_fields p,#content_wrap #pmpro_account p,#content_wrap #pmpro_account li,#content_wrap .pmpro_form p,#content_wrap .pmpro_form li,
+      .pmpro-confirmation #content_wrap .entry p,.pmpro-confirmation .entry li,.pmpro-invoice .entry li  { 
+	    font-weight: normal; font-style: normal;	}
+	#content_wrap .entry,article.post .entry, #slogan, .blog-unit-excerpt, .entry li,.text_block li,.gallery-info-property-con { font-family: Playfair Display; }
+    #content_wrap .entry,article.post .entry, #slogan, .blog-unit-excerpt, .entry li,.text_block li,.gallery-info-property-con { font-weight: normal; font-style: normal;}
+	.logo-h1 { font-family: Permanent Marker;}
+    .logo-h1 { font-size: 46px;}
+    .logo-h1 { font-weight: normal; font-style: normal;}
+    #navi a,.search-top-btn-class, #header-main .socialmeida-a,.subscribe-link-a { font-size: 18px;}
+    .hot-top-tit,.blog-unit-tit { font-size: 50px;}
+    #content_wrap .entry,.height-no-auto > .container,.fullwidth-wrap-inn,.full-half-inn,.blog-unit-excerpt,.hot-top-excerpt { font-size: 18px;}
+    .gallery-info-property,.blog-unit-meta-item,.content-post-meta-unit,.top-unit-meta,.blog-unit-meta,.post-navi-meta,.post-navi-meta-icon { font-size: 16px;}
+    .widget-title { font-size: 18px; }
+    ul.sidebar_widget > li { font-size: 16px;}
